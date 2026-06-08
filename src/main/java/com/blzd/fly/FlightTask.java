fc0d9c3254ded5dec2d6abc6d0d53fd74a39013b
Blzd <76827771+Blzd87@users.noreply.github.com>
2026-06-08 19:26:33 +1000

    Update FlightTask.java

--- src/main/java/com/blzd/fly/FlightTask.java
+++ src/main/java/com/blzd/fly/FlightTask.java
@@ -1,7 +1,7 @@
 package com.blzd.fly;
 
 import net.kyori.adventure.text.Component;
-import org.bukkit.Location;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import com.blzd.fly.FlightMode;
