1bbf217e92ce40e60a175ba26a1210feb9207c10
Blzd <76827771+Blzd87@users.noreply.github.com>
2026-06-08 13:54:53 +1000

    Create ClaimFlightTask.java

---
+++ src/main/java/com/blzd/fly/ClaimFlightTask.java
@@ -0,0 +1,38 @@
+package com.blzd.fly;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class ClaimFlightTask extends BukkitRunnable {
+
+    private final BlzdFly plugin;
+    private final ClaimManager claimManager = new ClaimManager();
+
+    public ClaimFlightTask(BlzdFly plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void run() {
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+
+            if (!plugin.isFlightEnabled(player.getUniqueId())) {
+                continue;
+            }
+
+            if (plugin.getFlightMode(player.getUniqueId())
+                    != FlightMode.CLAIM) {
+                continue;
+            }
+
+            if (!claimManager.canUseClaimFlight(player)) {
+
+                player.sendMessage(
+                        "§cDEBUG: Left valid claim."
+                );
+            }
+        }
+    }
+}
