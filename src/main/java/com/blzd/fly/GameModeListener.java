1ecbae1984d5420e023820241e4edb453333fe43
Blzd <76827771+Blzd87@users.noreply.github.com>
2026-06-29 19:47:56 +1000

    Create GameModeListener.java

---
+++ src/main/java/com/blzd/fly/GameModeListener.java
@@ -0,0 +1,35 @@
+package com.blzd.fly;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
+
+public class GameModeListener implements Listener {
+
+    private final BlzdFly plugin;
+
+    public GameModeListener(BlzdFly plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onGameModeChange(PlayerGameModeChangeEvent event) {
+
+        Player player = event.getPlayer();
+
+        Bukkit.getScheduler().runTask(plugin, () -> {
+
+            if (!plugin.isFlightEnabled(player.getUniqueId())) {
+                return;
+            }
+
+            player.setAllowFlight(true);
+
+            if (!player.isOnGround()) {
+                player.setFlying(true);
+            }
+        });
+    }
+}
