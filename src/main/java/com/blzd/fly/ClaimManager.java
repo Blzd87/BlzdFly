56fc4b39a3b351d0210139d90618980bb7baf75d
Blzd <76827771+Blzd87@users.noreply.github.com>
2026-07-01 18:43:07 +1000

    Update ClaimManager.java

--- src/main/java/com/blzd/fly/ClaimManager.java
+++ src/main/java/com/blzd/fly/ClaimManager.java
@@ -2,6 +2,7 @@ package com.blzd.fly;
 
 import com.griefdefender.api.GriefDefender;
 import com.griefdefender.api.claim.Claim;
+import com.griefdefender.api.permission.flag.TrustTypes;
 import org.bukkit.entity.Player;
 
 public class ClaimManager {
@@ -33,7 +34,7 @@ public class ClaimManager {
         }
     
         // Trusted player
-        return claim.isUserTrusted(player.getUniqueId());
+        return claim.isUserTrusted(player.getUniqueId(), TrustTypes.BUILDER);
     }                                                                                                                                                                                       
 
     public boolean canUseClaimFlight(Player player) {
