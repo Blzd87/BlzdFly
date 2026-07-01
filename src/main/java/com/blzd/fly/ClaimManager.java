5396b54439a20c39bdd8ab7c4b54164f358b8048
Blzd <76827771+Blzd87@users.noreply.github.com>
2026-07-01 18:37:07 +1000

    Update ClaimManager.java

--- src/main/java/com/blzd/fly/ClaimManager.java
+++ src/main/java/com/blzd/fly/ClaimManager.java
@@ -22,13 +22,19 @@ public class ClaimManager {
     public boolean isClaimOwner(Player player) {
 
         Claim claim = getClaim(player);
-
+    
         if (claim == null || claim.isWilderness()) {
             return false;
         }
-
-        return player.getUniqueId().equals(claim.getOwnerUniqueId());
-    }
+    
+        // Claim owner
+        if (player.getUniqueId().equals(claim.getOwnerUniqueId())) {
+            return true;
+        }
+    
+        // Trusted with build permission
+        return claim.canBuild(player);
+    }                                                                                                                                                                                        
 
     public boolean canUseClaimFlight(Player player) {
         return isClaimOwner(player);
